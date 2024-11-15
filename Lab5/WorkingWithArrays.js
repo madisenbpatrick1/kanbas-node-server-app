d484ed86062b72f57403f7b6c3d04f6e6e13a7d7
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-15 00:41:29 -0500

    a5_3.4.6 Updating Data on a Server

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -38,4 +38,10 @@ export default function WorkingWithArrays(app) {
     todos.splice(todoIndex, 1);
     res.json(todos);
   });
+  app.get("/lab5/todos/:id/title/:title", (req, res) => {
+    const { id, title } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todo.title = title;
+    res.json(todos);
+  });
 }
