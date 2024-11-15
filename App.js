d031f8ae511ebf0e65f90fb6b0e73d6d6e22e77a
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-15 12:27:45 -0500

    a5_3.5.2 Configuring Cross Origin Request Sharing (CORS)

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import express from 'express';
 import Hello from './Hello.js';
 import Lab5 from './Lab5/index.js';
 import PathParameters from './Lab5/PathParameters.js';
+import cors from "cors"
 // const express = require('express')
 const app = express()
-app.use(express.json());
+//app.use(express.json());
+app.use(cors())
 Lab5(app);
 
 Hello(app); // pass app reference to Hello
