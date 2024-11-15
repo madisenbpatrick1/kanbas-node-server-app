2430d555a60e72b5554a53e570bcb7206d9de150
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-14 23:33:49 -0500

    a5_3.3 Working with Remote Objects on a Server

--- Lab5/index.js
+++ Lab5/index.js
@@ -1,9 +1,11 @@
 import PathParameters from "./PathParameters.js";
 import QueryParameters from "./QueryParameters.js";
+import WorkingWithObjects from "./WorkingWithObjects.js";
 export default function Lab5(app) {
   app.get("/lab5/welcome", (req, res) => {
     res.send("Welcome to Lab 5");
   });
   PathParameters(app); // pass app reference to PathParameters
   QueryParameters(app); // pass app reference to QueryParameters
+  WorkingWithObjects(app); // pass app reference to WorkingWithObjects
 }
