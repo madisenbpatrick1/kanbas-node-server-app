abfceb0d43667564bd56444fbab20e2fd86a9915
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-17 18:38:34 -0500

    a5_4.2.1 Integrating the React Sign In Screen with a RESTful Web API

--- Kanbas/Database/index.js
+++ Kanbas/Database/index.js
@@ -4,4 +4,4 @@ import assignments from "./assignments.js";
 import users from "./users.js";
 import enrollments from "./enrollments.js";
 import grades from "./grades.js";
-export { courses, modules, assignments, users, enrollments, grades };
+export default { courses, modules, assignments, users, enrollments, grades };
--- index.js
+++ index.js
@@ -3,14 +3,20 @@ import Hello from './Hello.js';
 import Lab5 from './Lab5/index.js';
 import PathParameters from './Lab5/PathParameters.js';
 import cors from "cors"
+import UserRoutes from './Kanbas/Users/routes.js';
 // const express = require('express')
 const app = express()
+
 //app.use(express.json());
 app.use(cors())
 app.use(express.json());
+
+UserRoutes(app);
+
 Lab5(app);
 
 Hello(app); // pass app reference to Hello
  
 app.listen(process.env.PORT || 4000)
 
+// start with command line nodemon index.js
