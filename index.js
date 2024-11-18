726e324f2bfca93a49cdc5c7ebffde4aff9f3b32
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-17 19:03:12 -0500

    a5_4.3.1 Installing and Configuring Server Sessions

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
-import express from 'express';
-import Hello from './Hello.js';
-import Lab5 from './Lab5/index.js';
-import PathParameters from './Lab5/PathParameters.js';
-import cors from "cors"
-import UserRoutes from './Kanbas/Users/routes.js';
-// const express = require('express')
-const app = express()
+import "dotenv/config";
+import express from "express";
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
+import PathParameters from "./Lab5/PathParameters.js";
+import cors from "cors";
+import UserRoutes from "./Kanbas/Users/routes.js";
+import session from "express-session";
 
-//app.use(express.json());
-app.use(cors())
+const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+  })
+);
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }
+  app.use(session(sessionOptions));
+  
 
 UserRoutes(app);
 
 Lab5(app);
 
 Hello(app); // pass app reference to Hello
- 
-app.listen(process.env.PORT || 4000)
+
+app.listen(process.env.PORT || 4000);
 
 // start with command line nodemon index.js
