8cc931ecd91627d74fe4945a91c023d62977f16c
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-17 19:59:23 -0500

    a5_4.5.3 Deleting a Module

--- Kanbas/Modules/dao.js
+++ Kanbas/Modules/dao.js
@@ -9,3 +9,8 @@ export function createModule(module) {
   Database.modules = [...Database.modules, newModule];
   return newModule;
 }
+
+export function deleteModule(moduleId) {
+  const { modules } = Database;
+  Database.modules = modules.filter((module) => module._id !== moduleId);
+}
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import session from "express-session";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
 
 const app = express();
 app.use(express.json());
@@ -38,6 +39,7 @@ UserRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
+ModuleRoutes(app);
 app.listen(process.env.PORT || 4000);
 
 // start with command line nodemon index.js
