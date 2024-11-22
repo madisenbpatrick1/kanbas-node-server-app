5867e5a562f15125c62fd8c54fb456eb68d9e287
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-22 10:27:18 -0500

    update to NETLIFY_URL

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    origin: process.env.NETLIFY_URL || "http://localhost:3000" || process.env.NETLIFY_URL_A5,
   })
 );
 
