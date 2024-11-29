f365301c57fa8fd42e5a164efaae3377b5404428
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-29 10:49:10 -0500

    a6_5.1.3 Deleting Courses from the Database

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -9,15 +9,18 @@ export default function CourseRoutes(app) {
     const courses = await dao.findAllCourses();
     res.send(courses);
   });
+
   app.post("/api/courses", async (req, res) => {
     const course = await dao.createCourse(req.body);
     res.json(course);
   });
-  app.delete("/api/courses/:courseId", (req, res) => {
+
+  app.delete("/api/courses/:courseId", async (req, res) => {
     const { courseId } = req.params;
-    dao.deleteCourse(courseId);
-    res.sendStatus(204);
+    const status = await dao.deleteCourse(courseId);
+    res.sendStatus(status);
   });
+  
   app.put("/api/courses/:courseId", (req, res) => {
     const { courseId } = req.params;
     const courseUpdates = req.body;
