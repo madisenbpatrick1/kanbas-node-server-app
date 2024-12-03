f52a200e6511748afb966a61b4d09aba32ff44ad
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-12-03 18:53:44 -0500

    a6_5.2.5 Updating Modules

--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -8,10 +8,10 @@ export default function ModuleRoutes(app) {
     const status = await modulesDao.deleteModule(moduleId);
     res.send(status);
   });
-  app.put("/api/modules/:moduleId", (req, res) => {
+  app.put("/api/modules/:moduleId", async (req, res) => {
     const { moduleId } = req.params;
     const moduleUpdates = req.body;
-    modulesDao.updateModule(moduleId, moduleUpdates);
-    res.sendStatus(204);
+    const status = await modulesDao.updateModule(moduleId, moduleUpdates);
+    res.send(status);
   });
 }
