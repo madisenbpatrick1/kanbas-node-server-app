cc3be69b43642ff334b35df54da0a10b3a8f381c
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-12-03 18:15:37 -0500

    a6_5.2.3 Retrieving Modules for a Course

--- Kanbas/Modules/dao.js
+++ Kanbas/Modules/dao.js
@@ -1,14 +1,11 @@
-import Database from "../Database/index.js";
+//import Database from "../Database/index.js";
+import model from "./model.js";
+
 export function findModulesForCourse(courseId) {
-  const { modules } = Database;
-  return modules.filter((module) => module.course === courseId);
+  return model.find({ course: courseId });
 }
 
 export function createModule(module) {
-  // const newModule = { ...module, _id: Date.now().toString() };
-  // Database.modules = [...Database.modules, newModule];
-  // return newModule;
-  
   delete module._id;
   return model.create(module);
 }
