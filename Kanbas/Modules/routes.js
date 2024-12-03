cd5ac4e8906bb381c880249a589f9f8b349b2062
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-12-03 16:47:54 -0500

    Revert "a6_5.2 complete"
    This reverts commit 3500c0f7c2d779a034b5c7c0af77a9ff2bad71d2.

--- Kanbas/Modules/routes.js
+++ Kanbas/Modules/routes.js
@@ -1,14 +1,14 @@
 import * as modulesDao from "./dao.js";
 export default function ModuleRoutes(app) {
-  app.delete("/api/modules/:moduleId", async (req, res) => {
+  app.delete("/api/modules/:moduleId", (req, res) => {
     const { moduleId } = req.params;
-    const status = await modulesDao.deleteModule(moduleId);
-    res.send(status);
+    modulesDao.deleteModule(moduleId);
+    res.sendStatus(204);
   });
-  app.put("/api/modules/:moduleId", async(req, res) => {
+  app.put("/api/modules/:moduleId", (req, res) => {
     const { moduleId } = req.params;
     const moduleUpdates = req.body;
-    const status = await modulesDao.updateModule(moduleId, moduleUpdates);
-    res.send(status);
+    modulesDao.updateModule(moduleId, moduleUpdates);
+    res.sendStatus(204);
   });
 }
